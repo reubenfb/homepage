4a493c04c1e98e73e0244866ce986f4e505e25c7
reubenfb <reuben.fischerbaum@gmail.com>
2016-09-21 16:52:22 -0400

    fix column ordering bug

--- src/js/utils.js
+++ src/js/utils.js
@@ -11,20 +11,31 @@ module.exports = {
     var newData = [];
     var columnCount = 0;
 
-    _.each(data, function(chart){
-      var nextColumn = columnCount + 1;
-      if(nextColumn === columns){
-        nextColumn = 0;
-      }
-      if(counter[columnCount] <= counter[nextColumn]){
-        colArray[columnCount].push(chart);
-        counter[columnCount] = chart.interactive === "TRUE" ? counter[columnCount] + 2 : counter[columnCount] + 1;
+    _.each(data, function(chart, i){
+
+      if(i === 0){
+        colArray[0].push(chart);
+        counter[0] = chart.interactive === "TRUE" ? counter[0] + 2 : counter[columnCount] + 1;
       }
       else {
-        colArray[nextColumn].push(chart);
-        counter[nextColumn] = chart.interactive === "TRUE" ? counter[nextColumn] + 2 : counter[nextColumn] + 1;
+        assignColumn();
       }
-      columnCount = nextColumn;
+
+      function assignColumn() {
+
+        var prevColumn = columnCount - 1 < 0 ? columns - 1 : columnCount - 1;
+
+        if(counter[columnCount] < counter[prevColumn] || (columnCount === 0 && counter[columnCount] === counter[prevColumn])){
+          colArray[columnCount].push(chart);
+          counter[columnCount] = chart.interactive === "TRUE" ? counter[columnCount] + 2 : counter[columnCount] + 1;
+          columnCount = columnCount + 1 < columns ? columnCount + 1 : 0;
+          return;
+        } else {
+          columnCount = columnCount + 1 < columns ? columnCount + 1 : 0;
+          assignColumn();
+        }
+
+      }
     });
 
     return colArray[0].concat(colArray[1]).concat(colArray[2]).concat(colArray[3])
