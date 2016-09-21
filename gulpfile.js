ef52b1964d2d1f2ab4b94bbfcad2c4ef6b3a5088
Reuben <reuben.fischerbaum@gmail.com>
2016-09-20 20:10:30 -0400

    add webserver

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,21 @@
 var gulp = require('gulp');
 var deploy = require('gulp-gh-pages');
+var webserver = require('gulp-webserver')
+
+
+gulp.task('webserver', function() {
+  gulp.src('./public')
+    .pipe(webserver({
+      fallback: 'index.html',
+      livereload: true,
+      open: true,
+      port: 5380
+    }));
+});
 
 gulp.task('deploy', function () {
   return gulp.src("./public/**/*")
     .pipe(deploy())
 });
+
+gulp.task('default', ['webserver']);
