2cbb665d7a1bbee90b0471756f2c47b242c93670
Reuben <reuben.fischerbaum@gmail.com>
2016-10-01 17:29:02 -0400

    no hover on touchscreens

--- src/js/app.js
+++ src/js/app.js
@@ -2,13 +2,17 @@
 
 var d3 = require('d3-selection');
 
-d3.selectAll('.graphic')
-  .on('mouseover', function(){
-    d3.select(this).classed('userHover', true);
-  })
-  .on('mouseleave', function(){
-    d3.select(this).classed('userHover', false);
-  });
+if(window.innerWidth < 500 || ('ontouchstart' in window)|| (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0)){
+  return;
+} else {
+  d3.selectAll('.graphic')
+    .on('mouseover', function(){
+      d3.select(this).classed('userHover', true);
+    })
+    .on('mouseleave', function(){
+      d3.select(this).classed('userHover', false);
+    });
+}
 
 
 /* SAFARI UGH */
