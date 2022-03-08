e8b8fdf9e2cfef81eae2c1e7aecbc5df2edab61d
Reuben Fischer-Baum <reuben_fischer-baum@harvard.edu>
2022-03-08 15:35:42 -0500

    Tweaks

--- src/js/misc/knowlton.js
+++ src/js/misc/knowlton.js
@@ -3,9 +3,6 @@ var scale = require("d3-scale");
 
 let text = "Itwasafterdinner.Youweretalkingtomeacrossthetableaboutsomethingorother,agreyhoundyouhadseenthatdayorasongyouliked,andIwaslookingpastyouoveryourbareshoulderatthethreeorangeslyingonthekitchencounternexttothesmallelectricbeangrinder,whichwasalsoorange,andtheorangeandwhitecruetsforvinegarandoil.Allofwhichconvergedintoarandomstilllife,sofastenedtogetherbythehaspofcolor,andsofixedbehindtheanimatedforegroundofyourtalkingandsmiling,gesturingandpouringwine,andthecamberofyourshouldersthatIcouldfeelitbeingpaintedwithinme,brushedonthewallofmyskull,whilethetoneofyourvoiceliftedandfellinitsflight,andthethreeorangesremainedfixedonthecounterthewaystarsaresaidtobefixedintheuniverse.Thenallthemomentsofthepastbegantolineupbehindthatmomentandallthemomentstocomeassembledinfrontofitinalongrow,givingmereasontobelievethatthiswasamomentIhadrescuedfromthemillionsthatrushoutofsightintoadarknessbehindtheeyes.EvenafterIhaveforgottenwhatyearitis,mymiddlename,andthemeaningofmoney,Iwillstillcarryinmypocketthesmallcoinofthatmoment,mintedinthekingdomthatwepacethrougheveryday.PoemByBillyCollins1998"
 
-let startX = 1;
-let xPos = startX;
-let yPos = -5;
 let fontSize = 20;
 let charWidth = fontSize * 0.5;
 let lineHeight = fontSize * 0.75;
@@ -14,16 +11,16 @@ let height = Math.round(fontSize/30*610);
 let cols = 40;
 let rows = text.length/cols;
 
-
 let canvas = d3.select('#canvas').append('canvas')
 	.attr('width', width)
 	.attr('height', height);
 
 let context = canvas.node().getContext('2d');
+drawImage();
 
-make_base();
+function drawImage() {
 
-function make_base() {
+  // draw the image in Canvas
   base_image = new Image();
   base_image.src = 'images/brooke2.jpg';
   base_image.onload = function(){
@@ -52,7 +49,7 @@ function make_base() {
 		// calculcate lum
 		let lum = 0.299*r + 0.587*g + 0.114*b;
 
-		// figure out which character the pixel falls into
+		// assign the pixel to poem characters
 		let pixel = i/4;
 		let x = pixel % width;
 		let y = Math.floor(pixel/width);
@@ -64,15 +61,30 @@ function make_base() {
 		charValues[gridIndex].push(lum);
 	}
 
+	// find the average lum per character;
 	let avgLum = charValues.map(function(pixelArray){
 		return pixelArray.reduce((a, b) => a + b) / pixelArray.length;
 	});
 
+	drawPoem(avgLum, '#poem1', 2.2);
+	//drawPoem(avgLum, '#poem2', 2.5);
+  }
+}
+
+function drawPoem(avgLum, sel, pow){
+
+	let startX = 1;
+	let xPos = startX;
+	let yPos = -5;
+
+	// after playing around with it with some side-by-sides
+	// I got a sharper image from having lum be non-linear
+	avgLum = avgLum.map(lum => Math.pow(lum, pow));
+
 	let lumMin = Math.min(...avgLum);
 	let lumMax = Math.max(...avgLum)
 
-    // then actually draw the text
-	let poem = d3.select('.poem')
+	let poem = d3.select(sel)
 		.style('width', `${width}px`)
 		.style('height', `${height}px`);
 
@@ -82,7 +94,7 @@ function make_base() {
 
 	let opacityScale = scale.scaleLinear()
 		.domain([lumMin, lumMax])
-		.range([0.25, 1]);
+		.range([0.2, 1]);
 
 	for(let i = 0; i < text.length; i++){
 
@@ -102,9 +114,4 @@ function make_base() {
 	    xPos += charWidth;
 	  }
 	}
-
-  }
 }
-
-
-
