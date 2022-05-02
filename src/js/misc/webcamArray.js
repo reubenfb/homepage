3c8484023da620c64e3282fe59591b2e20272d62
Reuben Fischer-Baum <reuben_fischer-baum@harvard.edu>
2022-05-02 16:46:30 -0400

    get video switching working

--- src/js/misc/webcamArray.js
+++ src/js/misc/webcamArray.js
@@ -2,15 +2,18 @@ const sketch = (s) => {
 
 	let width = 320;
 	let capture;
+	let canvas;
+	let triggered = false;
 
 	// s.preload = () => {
 	// }
 
 	s.setup = () => {
-		s.createCanvas(width, width* 0.75);
+		canvas = s.createCanvas(width, width* 0.75);
 		s.pixelDensity(1);
 		capture = s.createCapture(s.VIDEO);
 		capture.hide();
+		canvas.mouseClicked(switchVideos);
 	};
 
 	s.draw = () => {
@@ -21,6 +24,37 @@ const sketch = (s) => {
 		s.pop();
 	};
 
+	function switchVideos(){
+
+		if(triggered == true){
+			return;
+		}
+
+		let vids = [1,2,3,4,5,6,7,8];
+		let randomCount = randomIntFromInterval(1,4);
+		let randomVids = vids.sort(() => .5 - Math.random()).slice(0,randomCount);
+
+		for(let i = 0; i<randomVids.length; i++){
+			let vid = document.getElementById(`vid${randomVids[i]}`);
+			vid.setAttribute('src', 'images/mp4s/try_start_small2.mp4');
+			vid.removeAttribute('loop');
+			triggered = true;
+
+			vid.onended = (event) => {
+				vid.setAttribute('src', 'images/mp4s/test_mosaic.mp4');
+				vid.setAttribute('loop', true);
+				triggered = false;
+			}
+
+			vid.load();
+		}
+
+	}
+
+	function randomIntFromInterval(min, max) { 
+		return Math.floor(Math.random() * (max - min + 1) + min)
+	}
+
 };
 
-let myp5 = new p5(sketch, window.document.getElementById('container'));
+let myp5 = new p5(sketch, document.getElementById('canvas-container'));
