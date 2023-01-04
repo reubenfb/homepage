0749b3fb89dfa9905b0aef4b3222ae3f628f67a2
Reuben Fischer-Baum <reuben_fischer-baum@harvard.edu>
2023-01-04 10:50:42 -0500

    get captures drawing on canvas

--- src/js/misc/webcam/face_average.js
+++ src/js/misc/webcam/face_average.js
@@ -18,6 +18,13 @@ const sketch = (s) => {
 	let noseY = 0;
 	let faceResult = null;
 
+	let fade = 0.2;
+	let increment = 0;
+	let heads = [];
+	let maxHeads = 4;
+	let headNum = 0;
+	let totalRuns = 0;
+
 	// if(navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/iPhone/i)){
 	// 	vidWidth = height * 3/4;
 	// 	offset = (width - vidWidth)/2;
@@ -26,8 +33,8 @@ const sketch = (s) => {
 
 	function loadModels(){
 	  Promise.all([
-	  	//faceapi.nets.tinyFaceDetector.load('../miscFiles/face-api-files/'),
-	    faceapi.nets.ssdMobilenetv1.load('../miscFiles/face-api-files/'),
+	  	faceapi.nets.tinyFaceDetector.load('../miscFiles/face-api-files/'),
+	    //faceapi.nets.ssdMobilenetv1.load('../miscFiles/face-api-files/'),
 	    faceapi.nets.faceLandmark68Net.load('../miscFiles/face-api-files/')
 	  ])
 	  // noLoop() was called in setup, pausing draw() while we load, we resume here once models are loaded
@@ -55,13 +62,14 @@ const sketch = (s) => {
 		loadModels();
 		// pause P5's update loop until the video is ready ('play' event)
 		s.noLoop();
+		s.noFill()
 
 	};
 
 	s.draw = async () => {
 
-		//faceapi.detectAllFaces(captureElement, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
-		faceapi.detectAllFaces(captureElement).withFaceLandmarks()
+		faceapi.detectAllFaces(captureElement, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
+		//faceapi.detectAllFaces(captureElement).withFaceLandmarks()
 			.then(result => drawFace(faceapi.resizeResults(result, displaySize)))
 
 		s.clear();
@@ -73,19 +81,58 @@ const sketch = (s) => {
 		s.pop();
 
 		if(faceResult){
-			for(let pos of faceResult){
-				s.circle(-(pos._x - width), pos._y, 5)
-			}
+
+			let eye1 = faceResult[39];
+			let eye2 = faceResult[42];
+			s.line(-(eye1._x -width), eye1._y, -(eye2._x - width), eye2._y)
+			s.circle(-(eye1._x - width), eye1._y, 5)
+			s.circle(-(eye2._x - width), eye2._y, 5)
+			// for(let pos of faceResult){
+			// 	s.circle(-(pos._x - width), pos._y, 5)
+			// 	//s.circle(pos._x, pos._y, 5)
+			// }
 		}
 
 		s.loadPixels();
 		let pixels = s.pixels;
+		increment++;
+
+		for(let i = 0; i < heads.length; i++){
+			if(heads[i]){
+				if(i == 0){
+					s.image(heads[0], 0, 0, 120, 120);
+				}
+				else if(i == 1){
+					s.image(heads[1], width - 120, 0, 120, 120);
+				}
+				else if(i == 2){
+					s.image(heads[2], width - 120, height - 120, 120, 120);
+				}
+				else {
+					s.image(heads[3], 0, height - 120, 120, 120);
+				}
+			}
+		}
 	};
 
-	let fade = 0.2;
-
 	function drawFace(result){
+
 		if(result[0]){
+
+			if(increment > 20 + (10 * (totalRuns + 1))){
+
+				let box = result[0].alignedRect._box;
+				let head = s.get(-(box._x - width) - box._width, box._y, box._width, box._height)
+
+				heads[headNum] = head;
+				totalRuns++
+				headNum++;
+				if(headNum === maxHeads){
+					headNum = 0;
+				}
+			}
+
+
 			if(!faceResult){
 				faceResult = result[0].landmarks.positions;
 			}
